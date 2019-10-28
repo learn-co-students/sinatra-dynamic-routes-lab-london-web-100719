4f0444b45365a014d49a5e6995be043c17ffe608
Liko <li_xie1992@hotmail.com>
2019-10-28 12:24:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,58 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse
+  end
 
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number * @number}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    
+    @string = ""
+    @number.times do 
+      @string = @string + @phrase
+    end
+    @string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @words = [
+      params[:word1], 
+      params[:word2], 
+      params[:word3], 
+      params[:word4], 
+      params[:word5]
+    ]
+    @string = ""
+
+    @words.each do |word|
+      word != @words.last ? @string = @string + word + " " : @string = @string + word + "."
+    end
+
+    @string
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    if @operation == "add"
+      result = @number1 + @number2
+    elsif @operation == "subtract"
+      result = @number1 - @number2
+    elsif @operation == "multiply"
+      result = @number1 * @number2
+    elsif @operation == "divide"
+      result = @number1 / @number2
+    end
+    result.to_s
+  end
 end
